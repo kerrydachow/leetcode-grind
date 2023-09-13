34f96bc964c8c25662f0711999557e235e0d6ede
kerrydachow <kerry_chow@hotmail.ca>
2023-09-13 15:01:06 -0700

    Add 1984-minimum-difference-between-highest-and-lowest-of-k-scores.py

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumDifference(self, nums: list[int], k: int) -> int:
+        """
+        Key Takeaways
+        -------------
+        -   Minimum possible difference of a window of length k
+        -   Sort the array
+        -   The closer the numbers are in value, the smaller
+            the difference will be
+
+        Complexity Analysis
+        -------------------
+        Time Complexity: O(nlogn)
+            -   sort: O(nlogn)
+            -   one pass: O(n)
+
+        Space Complexity: O(1)
+            -   no extra space required
+        """
+        nums.sort()
+        i = 0
+        res = float("inf")
+        for j in range(k - 1, len(nums)):
+            res = min(res, nums[j] - nums[i])
+        return res
