db2131faf605eb6f40fa58c52bbb1310231853b6
kerrydachow <kerry_chow@hotmail.ca>
2023-09-17 18:48:41 -0700

    Add 0881-boats-to-save-people.py

---
+++ 0881-boats-to-save-people.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numRescueBoats(self, people: list[int], limit: int) -> int:
+        """
+        Key Takeaways
+        -------------
+        -   Sort the list
+        -   Two pointers
+        -   Pair the heaviest, with lightest
+        -   If a pair can go on a boat, increment i
+        -   Else always increment j
+
+        Complexity Analysis
+        -------------------
+        Time Complexity: O(nlogn)
+            -   sort: O(nlogn)
+            -   2 pointers: O(n)
+
+        Space Complexity: O(1)
+            -   no extra space required
+        """
+        people.sort()
+        res = 0
+        i, j = 0, len(people) - 1
+        while i <= j:
+            curr_sum = people[i] + people[j]
+            if curr_sum <= limit:
+                i += 1
+            j -= 1
+            res += 1
+        return res
