934ec890e3e3308dc3eca3799f6e1d13868f321a
kerrydachow <kerry_chow@hotmail.ca>
2023-09-30 13:37:35 -0700

    Add 0735-asteroid-collision.py 2

--- 0735-asteroid-collision.py
+++ 0735-asteroid-collision.py
@@ -33,3 +33,38 @@ class Solution:
             else:
                 res.append(a)
         return res
+
+
+class Solution:
+    """
+    Key Takeaways
+    -------------
+    -   Without break statements
+    -   Stack
+    -   Reassign asteroid to 0 when destroyed
+
+    Complexity Analysis
+    -------------------
+    Time Complexity: O(n)
+        -   one pass: O(n)
+        -   while loop executed at most n time: O(n)
+
+    Space Complexity: O(n)
+        -   stack used for output may have N elements
+    """
+    def asteroidCollision(self, asteroids: list[int]) -> list[int]:
+        res = []
+        for a in asteroids:
+            # Collision
+            while res and res[-1] > 0 and a < 0:
+                diff = res[-1] + a
+                if diff < 0:  # destroyed +asteroid
+                    res.pop()
+                elif diff > 0:  # -asteroid destroyed
+                    a = 0
+                else:  # both +-asteroid destroyed
+                    a = 0
+                    res.pop()
+            if a:  # only append if new asteroid exists; > 0
+                res.append(a)
+        return res
